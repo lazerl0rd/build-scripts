b2f31cc611b7de21ef7eae76a2db34ded6a87bfb
Diab Neiroukh <officiallazerl0rd@gmail.com>
2020-03-10 18:24:42 +0000

    🐛 FIX: Use correct string for building Release on CMake

--- hello.sh
+++ hello.sh
@@ -9,7 +9,7 @@ git pull >/dev/null
 rm -rf ./build
 mkdir -p ./build
 cd ./build
-cmake -DCMAKE_BUILD_TYPE=release -DCMAKE_INSTALL_PREFIX=/usr ..
+cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=/usr ..
 AR=llvm-ar CC=clang CXX=clang++ CFLAGS="-O3 -march=native -mllvm -polly -Wp,-D_FORTIFY_SOURCE=2" NM=llvm-nm OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump STRIP=llvm-strip make -j"$(nproc)"
 sudo make install
 popd || exit
--- kwin-lowlatency.sh
+++ kwin-lowlatency.sh
@@ -9,7 +9,7 @@ git pull >/dev/null
 rm -rf ./build
 mkdir -p ./build
 cd ./build
-cmake -DCMAKE_BUILD_TYPE=release -DCMAKE_INSTALL_PREFIX=/usr -DCMAKE_INSTALL_LIBDIR=lib/x86_64-linux-gnu -DCMAKE_INSTALL_LIBEXECDIR=lib/x86_64-linux-gnu -DBUILD_TESTING=OFF ..
+cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=/usr -DCMAKE_INSTALL_LIBDIR=lib/x86_64-linux-gnu -DCMAKE_INSTALL_LIBEXECDIR=lib/x86_64-linux-gnu -DBUILD_TESTING=OFF ..
 AR=llvm-ar CC=clang CXX=clang++ CFLAGS="-O3 -march=native -mllvm -polly -Wp,-D_FORTIFY_SOURCE=2" NM=llvm-nm OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump STRIP=llvm-strip make -j"$(nproc)"
 sudo make install
 popd || exit
